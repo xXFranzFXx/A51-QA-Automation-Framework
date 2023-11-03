2e01d7cf3141c10028fb2c3ceba3b3242bd56902
FJF616 <franz.fernando@testpro.io>
2023-11-03 11:47:10 -0700

    sets up helper methods for playSong test case

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,10 +1,11 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class Homework18 extends BaseTest {
     @Test
     public void playSong() {
-        
-    }
+
     //launch browser
 
     //login: email, password, submit
@@ -14,4 +15,23 @@ public class Homework18 extends BaseTest {
     //play button
 
     //Assertions
+    }
+    /**************************    helper methods  *************************************************/
+    //check if soundBar element is displayed
+    private boolean isSoundPlaying() {
+        WebElement soundBar = driver.findElement(By.cssSelector("[data-testid='sound-bar-play']"));
+        //boolean requires return value, check if element is displayed
+        return soundBar.isDisplayed();
+    }
+    //click play button
+    public void clickPlay() {
+        WebElement playButton = driver.findElement(By.cssSelector("[data-testid='play-btn']"));
+        playButton.click();
+    }
+    //click play next button
+    private void playNextSong() {
+        WebElement playNextButton = driver.findElement(By.cssSelector("[data-testid='play-next-btn']"));
+        playNextButton.click();
+    }
+
 }
