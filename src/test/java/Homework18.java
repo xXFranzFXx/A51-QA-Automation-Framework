12593670d1178959d2c49480819502b151ba1968
FJF616 <franz.fernando@testpro.io>
2023-11-03 11:38:13 -0700

    sets up @Test playSong method, configures TestNG xml

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,3 +1,17 @@
+import org.testng.annotations.Test;
+
 public class Homework18 extends BaseTest {
-    
+    @Test
+    public void playSong() {
+        
+    }
+    //launch browser
+
+    //login: email, password, submit
+
+    //play next button
+
+    //play button
+
+    //Assertions
 }
