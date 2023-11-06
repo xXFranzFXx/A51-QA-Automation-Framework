2592376ed391bceca96f1dd114f975b9a9bfe030
FJF616 <franz.fernando@testpro.io>
2023-11-06 15:47:09 -0800

    refactors BasePage to SPF

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,24 +13,31 @@ import java.time.Duration;
 //POM
 public class BasePage {
     //POM attributes
-    WebDriver driver;
-    WebDriverWait wait;
-    Actions actions;
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected Actions actions;
 
     //constructor method
     public BasePage(WebDriver givenDriver) {
         driver = givenDriver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(10L));
-        actions = new Actions(driver);
+//        actions = new Actions(driver);
+        PageFactory.initElements(driver, this);
     }
 
     //reusable methods, inherited by other pages
 
-    public WebElement findElement(By locator) {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    protected WebElement findElement(WebElement webElement) {
+        return wait.until(ExpectedConditions.visibilityOf(webElement));
     }
 
-    public void clickable(By locator) {
-        wait.until(ExpectedConditions.elementToBeClickable(locator));
+    protected void click(WebElement webElement) {
+        wait.until(ExpectedConditions.elementToBeClickable(webElement)).click();
+    }
+    protected void contextClick(WebElement webElement) {
+        actions.contextClick(findElement(webElement)).perform();
+    }
+    protected void doubleClick(WebElement webElement) {
+        actions.doubleClick(findElement(webElement)).perform();
     }
 }
