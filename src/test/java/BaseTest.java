d5a7f980b05bbbc99f6f0220c6aa2a87e090c5b5
FJF616 <franz.fernando@testpro.io>
2023-11-06 13:33:52 -0800

    creates renamePlaylist method

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -19,6 +20,7 @@ public class BaseTest {
     //constant variables used by helper methods
     public WebDriver driver;
     public WebDriverWait wait;
+    public Actions action;
     public String url;
     //TestNG decorators to be run for each test
 
@@ -37,6 +39,7 @@ public class BaseTest {
         driver = new ChromeDriver(options);
 //        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10L));
         wait = new WebDriverWait(driver, Duration.ofSeconds(10L));
+        action = new Actions(driver);
         url = baseURL;
         navigateToLoginPage();
     }
