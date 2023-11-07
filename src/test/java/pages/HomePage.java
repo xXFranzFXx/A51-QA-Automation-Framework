76fcdefc2056e6112a5b5ce180214dc201a4da2c
FJF616 <franz.fernando@testpro.io>
2023-11-06 16:05:34 -0800

    Refactors HomePage and LoginPage for SPF

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,15 +3,17 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage extends BasePage {
-    By userAvatarIcon = By.cssSelector("img.avatar");
+    @FindBy(css = "img.avatar")
+    private WebElement userAvatarIcon;
     public HomePage(WebDriver givenDriver) {
         super(givenDriver);
     }
 
     public WebElement getUserAvatar () {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(userAvatarIcon));
+         return wait.until(ExpectedConditions.visibilityOf(userAvatarIcon));
     }
 }
