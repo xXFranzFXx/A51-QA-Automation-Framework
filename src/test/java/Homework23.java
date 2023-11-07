f41e1f534717165557221e9f9d9eb0cbc0027d60
FJF616 <franz.fernando@testpro.io>
2023-11-06 16:07:39 -0800

    reuses homework22 for SPF test case

---
+++ src/test/java/Homework23.java
@@ -0,0 +1,24 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pages.HomePage;
+import pages.LoginPage;
+
+
+
+public class Homework23 extends BaseTest {
+    @Test
+    public void loginValidEmailPasswordTest() {
+
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+
+        loginPage.provideEmail("fake@fakeaccount.com");
+        loginPage.providePassword("te$t$tudent");
+        loginPage.clickSubmitBtn();
+
+        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+
+    }
+
+}
