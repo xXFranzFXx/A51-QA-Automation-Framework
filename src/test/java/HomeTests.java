35d3e6c30f285e35162cd4d7fedc7751170e4c3c
xXFranzFXx <franz.fernando@testpro.io>
2023-12-05 17:10:13 -0800

    syntax changes

--- src/test/java/HomeTests.java
+++ src/test/java/HomeTests.java
@@ -4,23 +4,11 @@ import pages.HomePage;
 import pages.LoginPage;
 
 public class HomeTests extends BaseTest{
-    LoginPage loginPage = new LoginPage(getDriver());
-    HomePage homePage = new HomePage(getDriver());
+//    LoginPage loginPage = new LoginPage(getDriver());
+//    HomePage homePage = new HomePage(getDriver());
     private final String searchArtist = "Grav";
 
-    @Test
-    public void loginValidEmailPasswordTest() {
 
-        LoginPage loginPage = new LoginPage(getDriver());
-        HomePage homePage = new HomePage(getDriver());
-
-        loginPage.provideEmail("fake@fakeaccount.com")
-                .providePassword("te$t$tudent")
-                .clickSubmitBtn();
-
-        Assert.assertTrue(homePage.getUserAvatar());
-
-    }
 //    public void addSongToPlaylist() {
 //        loginPage.loginValidCredentials();
 //        homePage.searchSong("dark")
@@ -36,48 +24,56 @@ public class HomeTests extends BaseTest{
 //        loginPage.loginValidCredentials();
 //        Assert.assertTrue(homePage.hoverPlay());
 //    }
-//    @Test
-//    //checks visibility/invisibility of info panel by clicking INFO button
-//    public void checkInfoPanelVisible () {
-//       loginPage.loginValidCredentials();
-//       if (homePage.isInfoPanelVisible()){
-//           homePage.clickInfoButton();
-//           Assert.assertFalse(homePage.isInfoPanelVisible());
-//       } else {
-//           homePage.clickInfoButton();
-//           Assert.assertTrue(homePage.isInfoPanelVisible());
-//       }
-//    }
-//    @Test
-//    public void checkLyricsInfoPanelTab() {
-//        loginPage.loginValidCredentials();
-//
-//        homePage.searchSong(searchArtist)
-//                .doubleClickFirstSearchResult()
-//                .clickInfoButton()
-//                .clickLyricsTab();
-//        String lyricsInfoText = "No lyrics available. Are you listening to Bach?";
-//        Assert.assertEquals(homePage.getLyricsText().toString(), lyricsInfoText);
-//    }
-//    @Test
-//    public void checkArtistInfoPanelTab() {
-//        loginPage.loginValidCredentials();
-////        String searchArtist = "Grav";
-//        homePage.searchSong(searchArtist)
-//                .doubleClickFirstSearchResult()
-//                .clickInfoButton()
-//                .clickArtistTab();
-//        Assert.assertEquals(homePage.getArtistTabText().toString(), searchArtist);
-//    }
-//    @Test
-//    public void checkAlbumInfoPanelTab() {
-//        loginPage.loginValidCredentials();
-//        String searchResultSong = homePage.getSearchResultSongText().toString();
-//        String albumInfoPanelText = homePage.getAlbumTabText().toString();
-//        homePage.searchSong(searchArtist)
-//                .doubleClickFirstSearchResult()
-//                .clickInfoButton()
-//                .clickAlbumTab();
-//        Assert.assertTrue(albumInfoPanelText.contains(searchResultSong));
-//    }
+    @Test
+    //checks visibility/invisibility of info panel by clicking INFO button
+    public void checkInfoPanelVisible () {
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+       loginPage.loginValidCredentials();
+       if (homePage.isInfoPanelVisible()){
+           homePage.clickInfoButton();
+           Assert.assertFalse(homePage.isInfoPanelVisible());
+       } else {
+           homePage.clickInfoButton();
+           Assert.assertTrue(homePage.isInfoPanelVisible());
+       }
+    }
+    @Test
+    public void checkLyricsInfoPanelTab() {
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+        loginPage.loginValidCredentials();
+
+        homePage.searchSong(searchArtist)
+                .doubleClickFirstSearchResult()
+                .clickInfoButton()
+                .clickLyricsTab();
+        String lyricsInfoText = "No lyrics available. Are you listening to Bach?";
+        Assert.assertEquals(homePage.getLyricsText().toString(), lyricsInfoText);
+    }
+    @Test
+    public void checkArtistInfoPanelTab() {
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+        loginPage.loginValidCredentials();
+//        String searchArtist = "Grav";
+        homePage.searchSong(searchArtist)
+                .doubleClickFirstSearchResult()
+                .clickInfoButton()
+                .clickArtistTab();
+        Assert.assertEquals(homePage.getArtistTabText().toString(), searchArtist);
+    }
+    @Test
+    public void checkAlbumInfoPanelTab() {
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
+        loginPage.loginValidCredentials();
+        String searchResultSong = homePage.getSearchResultSongText().toString();
+        String albumInfoPanelText = homePage.getAlbumTabText().toString();
+        homePage.searchSong(searchArtist)
+                .doubleClickFirstSearchResult()
+                .clickInfoButton()
+                .clickAlbumTab();
+        Assert.assertTrue(albumInfoPanelText.contains(searchResultSong));
+    }
 }
