30f872341c48eca96fc8ad785b19bf3daff9e84b
xXFranzFXx <franz.fernando@testpro.io>
2023-12-22 08:26:31 -0800

    syntax corrections

--- src/test/java/testRunners/CucumberRunner.java
+++ src/test/java/testRunners/CucumberRunner.java
@@ -5,11 +5,14 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.TestNGCucumberRunner;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.annotations.AfterClass;
@@ -18,7 +21,8 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
-
+@RunWith(Cucumber.class)
+@Test
 @CucumberOptions (
             features = {"src/test/resources/features"},
             glue= "stepDefinitions",
@@ -39,6 +43,7 @@ import java.net.MalformedURLException;
             testNGCucumberRunner =  new TestNGCucumberRunner(this.getClass());
         }
 
+
         @DataProvider
         public Object[][] features() {
             return testNGCucumberRunner.provideScenarios();
@@ -50,6 +55,13 @@ import java.net.MalformedURLException;
 //        public Object [][] scenarios() {
 //            return super.scenarios();
 //        }
+//@AfterStep
+//public void addScreenshot(Scenario scenario){
+//    final byte[] screenshot = ((TakesScreenshot) BaseDefinitions.getDriver()).getScreenshotAs(OutputType.BYTES);
+//    scenario.attach(screenshot, "image/png", "image");
+//
+//}
+
         @AfterClass(alwaysRun = true)
         public void tearDownThisClass() {
             testNGCucumberRunner.finish();
