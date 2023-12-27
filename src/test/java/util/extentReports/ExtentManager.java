30645751483f5e7bff3ffd924d26e517fda4a7a4
xXFranzFXx <franz.fernando@testpro.io>
2023-12-26 19:20:34 -0800

    updates gradle dependency for extent reports

--- src/test/java/util/extentReports/ExtentManager.java
+++ src/test/java/util/extentReports/ExtentManager.java
@@ -1,8 +1,7 @@
 package util.extentReports;
 
 import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 import org.openqa.selenium.Platform;
 
@@ -25,10 +24,10 @@ public class ExtentManager {
     public static ExtentReports createInstance() {
         platform = getCurrentPlatform();
         String fileName = getReportFileLocation(platform);
-        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(fileName);
-        htmlReporter.config().setTestViewChartLocation(ChartLocation.BOTTOM);
-        htmlReporter.config().setChartVisibilityOnOpen(true);
-        htmlReporter.config().setTheme(Theme.STANDARD);
+        ExtentSparkReporter htmlReporter = new ExtentSparkReporter(fileName);
+//        htmlReporter.config().setTestViewChartLocation(ChartLocation.BOTTOM);
+//        htmlReporter.config().setChartVisibilityOnOpen(true);
+        htmlReporter.config().setTheme(Theme.DARK);
         htmlReporter.config().setDocumentTitle(fileName);
         htmlReporter.config().setEncoding("utf-8");
         htmlReporter.config().setReportName(fileName);
