0ae96703fff5953acca4f84c02c82ccac031bb02
xXFranzFXx <franz.fernando@testpro.io>
2024-01-11 16:31:02 -0800

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -14,6 +14,9 @@ public class HomePage extends BasePage {
 
     @FindBy(xpath = "//a[@href='/#!/profile']")
     private WebElement profilePageLink;
+    @FindBy(xpath = "//div[@id='searchForm']/input[@type='search']")
+    private WebElement searchSongInput;
+
 
     public HomePage(WebDriver givenDriver) {
         super(givenDriver);
@@ -23,7 +26,10 @@ public class HomePage extends BasePage {
     public boolean getUserAvatar() {
         return userAvatarIcon.isEnabled();
     }
-
+    public void searchSong(String song) {
+        searchSongInput.clear();
+        searchSongInput.sendKeys(song);
+    }
 
     public void clickAvatar() {
         findElement(profilePageLink).click();
